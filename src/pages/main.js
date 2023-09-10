1a18dd9d97b2bf5a995848b6356e67ae0351cbfd
letsjo <hyunoh.jo@gmail.com>
2023-09-11 00:56:14 +0900

    Feat: add main page in rander

---
+++ src/pages/main.js
@@ -0,0 +1,19 @@
+function Main($container) {
+  this.$container = $container;
+
+  this.setState = () => {
+    this.render();
+  };
+
+  this.render = () => {
+    this.$container.innerHTML = `
+      <main class="mainPage">
+        메인 페이지입니다.
+      </main>
+    `;
+  };
+
+  this.render();
+}
+
+export default Main;
